c5ddc714ca7a803a6e055fd1597361a553f06d26
alexandre <alexandre.pisani.ant@gmail.com>
2023-10-09 13:03:30 -0300

    service layer

--- src/controllers/diagramController.ts
+++ src/controllers/diagramController.ts
@@ -1,48 +1,31 @@
-import fetchFirebaseDataMatch, {
-    addFirebaseDocument,
-    fetchAllFirebaseData,
-    deleteFirebaseDoc,
-    updateFirebaseDocument
-} from "../config/fetchFirebaseData"
-import { v4 as uuidv4 } from "uuid"
-import { DiagramSchema } from "../types"
+import { DiagramSchema } from '../types'
+import {
+    createDiagramService,
+    listDiagramsByIdService,
+    listDiagramsService,
+    editDiagramsService,
+    deleteDiagramsService
+} from '../services/diagramService'
 
 export const createDiagram = async (diagramData: DiagramSchema) => {
     try {
-        const id = uuidv4()
-        diagramData["id"] = id
-        diagramData["name"] = "workflow" + id.substring(0, 8)
-        diagramData["creation"] = new Date().toISOString()
-        await addFirebaseDocument(diagramData, process.env.COLLECTION_NAME)
-        return diagramData
-    } catch (e) {
-        console.error("Error when creating diagram :  ", e)
+        return await createDiagramService(diagramData)
+    } catch (error) {
+        throw new Error('Error creating diagram: ' + error.message)
     }
 }
 export const listDiagramsById = async (field: string, fieldValue: string) => {
     try {
-        const respAlldiagrams = await fetchFirebaseDataMatch(
-            process.env.COLLECTION_NAME,
-            field,
-            fieldValue,
-            field,
-            "true"
-        )
-        return respAlldiagrams
-    } catch (e) {
-        console.error("Error when listing diagrams :  ", e)
+        return await listDiagramsByIdService(field, fieldValue)
+    } catch (error) {
+        throw new Error('Error listing diagrams: ' + error.message)
     }
 }
 export const listDiagrams = async () => {
     try {
-        const respAlldiagrams = await fetchAllFirebaseData(
-            process.env.COLLECTION_NAME,
-            "name",
-            "true"
-        )
-        return respAlldiagrams
-    } catch (e) {
-        console.error("Error when listing diagrams :  ", e)
+        return await listDiagramsService()
+    } catch (error) {
+        throw new Error('Error listing diagrams: ' + error.message)
     }
 }
 export const editDiagrams = async (
@@ -51,25 +34,15 @@ export const editDiagrams = async (
     body: object
 ) => {
     try {
-        const respFetchByField = await updateFirebaseDocument(
-            process.env.COLLECTION_NAME,
-            field,
-            fieldValue,
-            body
-        )
-        return respFetchByField
-    } catch (e) {
-        console.error("Error when editing diagrams :  ", e)
+        return await editDiagramsService(field, fieldValue, body)
+    } catch (error) {
+        throw new Error('Error editing diagrams: ' + error.message)
     }
 }
-export const deleteDiagrams = (fieldName: string, fieldValue: string) => {
+export const deleteDiagrams = async (fieldName: string, fieldValue: string) => {
     try {
-        return deleteFirebaseDoc(
-            process.env.COLLECTION_NAME,
-            fieldName,
-            fieldValue
-        )
-    } catch (e) {
-        console.error("Error when deleting Diagrams :  ", e)
+        return await deleteDiagramsService(fieldName, fieldValue)
+    } catch (error) {
+        throw new Error('Error deleting diagrams: ' + error.message)
     }
 }
--- src/services/diagramService.ts
+++ src/services/diagramService.ts
@@ -1,3 +1,85 @@
-// const diagramService = () => {
+import fetchFirebaseDataMatch, {
+    addFirebaseDocument,
+    fetchAllFirebaseData,
+    deleteFirebaseDoc,
+    updateFirebaseDocument
+} from '../config/fetchFirebaseData'
+import { v4 as uuidv4 } from 'uuid'
+import { DiagramSchema } from '../types'
 
-// }
+export const createDiagramService = async (diagramData: DiagramSchema) => {
+    try {
+        const id = uuidv4()
+        diagramData['id'] = id
+        diagramData['name'] = 'workflow' + id.substring(0, 8)
+        diagramData['creation'] = new Date().toISOString()
+        await addFirebaseDocument(diagramData, process.env.COLLECTION_NAME)
+        return diagramData
+    } catch (e) {
+        console.error('Error when creating diagram :  ', e)
+    }
+}
+
+export const listDiagramsByIdService = async (
+    field: string,
+    fieldValue: string
+) => {
+    try {
+        const respAlldiagrams = await fetchFirebaseDataMatch(
+            process.env.COLLECTION_NAME,
+            field,
+            fieldValue,
+            field,
+            'true'
+        )
+        return respAlldiagrams
+    } catch (e) {
+        console.error('Error when listing diagrams :  ', e)
+    }
+}
+
+export const listDiagramsService = async () => {
+    try {
+        const respAlldiagrams = await fetchAllFirebaseData(
+            process.env.COLLECTION_NAME,
+            'name',
+            'true'
+        )
+        return respAlldiagrams
+    } catch (e) {
+        console.error('Error when listing diagrams :  ', e)
+    }
+}
+
+export const editDiagramsService = async (
+    field: string,
+    fieldValue: string,
+    body: object
+) => {
+    try {
+        const respFetchByField = await updateFirebaseDocument(
+            process.env.COLLECTION_NAME,
+            field,
+            fieldValue,
+            body
+        )
+        return respFetchByField
+    } catch (e) {
+        console.error('Error when editing diagrams :  ', e)
+    }
+}
+
+export const deleteDiagramsService = async (
+    fieldName: string,
+    fieldValue: string
+) => {
+    try {
+        return deleteFirebaseDoc(
+            process.env.COLLECTION_NAME,
+            fieldName,
+            fieldValue
+        )
+    } catch (e) {
+        console.error('Error when deleting Diagrams :  ', e)
+    }
+}
